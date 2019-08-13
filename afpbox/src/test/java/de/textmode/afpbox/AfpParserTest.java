ecac7119144258f9a4066923c7c19f16e71149ab
Michael Knigge <michael.knigge@gmx.de>
2019-08-13 13:28:06 +0200

    Added unit tests for AfpParser

---
+++ afpbox/src/test/java/de/textmode/afpbox/AfpParserTest.java
@@ -0,0 +1,113 @@
+package de.textmode.afpbox;
+
+import java.io.IOException;
+import java.util.LinkedList;
+
+import org.apache.commons.codec.binary.Hex;
+
+import de.textmode.afpbox.common.StructuredFieldIdentifier;
+import de.textmode.afpbox.io.Record;
+import de.textmode.afpbox.io.RecordReader;
+import junit.framework.TestCase;
+
+public final class AfpParserTest extends TestCase implements RecordReader, RecordHandler {
+
+    private final LinkedList<Record> records = new LinkedList<>();
+    private final LinkedList<Record> handledLineRecords = new LinkedList<>();
+    private final LinkedList<StructuredFieldIntroducer> handledStructuredFieldIntroducers = new LinkedList<>();
+    private final LinkedList<StructuredField> handledStructuredFields = new LinkedList<>();
+
+    @Override
+    public void setUp() {
+        this.records.clear();
+        this.handledLineRecords.clear();
+        this.handledStructuredFieldIntroducers.clear();
+        this.handledStructuredFields.clear();
+    }
+
+    /**
+     * Processes an empty file.
+     */
+    public void testEmptyFile() throws Exception {
+        new AfpParser(this, this).parse();
+
+        assertTrue(this.records.isEmpty());
+        assertTrue(this.handledLineRecords.isEmpty());
+        assertTrue(this.handledStructuredFieldIntroducers.isEmpty());
+        assertTrue(this.handledStructuredFields.isEmpty());
+    }
+
+    /**
+     * Processes only line mode records.
+     */
+    public void testOnlyLineRecords() throws Exception {
+        // Two simple Line Mode Records (with ASA control characters and three EBCDIC spaces)
+        final Record record1 = new Record(Hex.decodeHex("F1404040".toCharArray()), 0);
+        final Record record2 = new Record(Hex.decodeHex("40404040".toCharArray()), 1);
+
+        this.records.add(record1);
+        this.records.add(record2);
+
+        new AfpParser(this, this).parse();
+
+        assertEquals(this.handledLineRecords.poll(), record1);
+        assertEquals(this.handledLineRecords.poll(), record2);
+        assertEquals(this.handledLineRecords.poll(), null);
+
+        assertTrue(this.handledStructuredFieldIntroducers.isEmpty());
+        assertTrue(this.handledStructuredFields.isEmpty());
+    }
+
+    /**
+     * Processes a mixed-mode file.
+     */
+    public void testMixedModeFile() throws Exception {
+        // AFP records and simple Line Mode Records (with ASA control characters and three EBCDIC spaces)
+        final Record nopRecord1 = new Record(Hex.decodeHex("5A0008D3A8A5000000".toCharArray()), 0);
+        final Record lineRecord1 = new Record(Hex.decodeHex("F1404040".toCharArray()), 1);
+        final Record lineRecord2 = new Record(Hex.decodeHex("40404040".toCharArray()), 2);
+        final Record nopRecord2 = new Record(Hex.decodeHex("5A000BD3EEEE000000F1F2F3".toCharArray()), 3);
+
+        this.records.add(nopRecord1);
+        this.records.add(lineRecord1);
+        this.records.add(lineRecord2);
+        this.records.add(nopRecord2);
+
+        new AfpParser(this, this).parse();
+
+        assertEquals(this.handledLineRecords.poll(), lineRecord1);
+        assertEquals(this.handledLineRecords.poll(), lineRecord2);
+        assertEquals(this.handledLineRecords.poll(), null);
+
+        final StructuredFieldIntroducer sfi1 = this.handledStructuredFieldIntroducers.poll();
+        assertEquals(sfi1.getStructuredFieldIdentifier(), StructuredFieldIdentifier.BPF);
+
+        final StructuredFieldIntroducer sfi2 = this.handledStructuredFieldIntroducers.poll();
+        assertEquals(sfi2.getStructuredFieldIdentifier(), StructuredFieldIdentifier.NOP);
+
+        assertEquals(this.handledStructuredFieldIntroducers.poll(), null);
+
+        assertTrue(this.handledStructuredFields.isEmpty());
+    }
+
+    @Override
+    public Record read() throws IOException {
+        return this.records.poll();
+    }
+
+    @Override
+    public void handleLineRecord(final Record record) {
+        this.handledLineRecords.add(record);
+    }
+
+    @Override
+    public boolean handleStructuredFieldIntroducer(final StructuredFieldIntroducer sfi) {
+        this.handledStructuredFieldIntroducers.add(sfi);
+        return false;
+    }
+
+    @Override
+    public void handleStructuredField(final StructuredField sf) {
+        this.handledStructuredFields.add(sf);
+    }
+}
