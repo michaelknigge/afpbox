d75ea7ef3df156d415ae490efd65e95f095046de
Michael Knigge <michael.knigge@gmx.de>
2019-08-13 12:40:26 +0200

    Added new class StructuredFieldIntroducer

---
+++ afpbox/src/main/java/de/textmode/afpbox/StructuredFieldIntroducer.java
@@ -0,0 +1,154 @@
+package de.textmode.afpbox;
+
+/*
+ * Copyright 2019 Michael Knigge
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import de.textmode.afpbox.common.ByteUtils;
+import de.textmode.afpbox.io.Record;
+
+/**
+ * The {@link StructuredFieldIntroducer} contains the Structured Field Introducer that follows the 0x5A
+ * control character of an AFP record.
+ */
+public final class StructuredFieldIntroducer {
+
+    private static final int BIT0 = 0x80; // 1000 0000
+    private static final int BIT2 = 0x20; // 0010 0000
+    private static final int BIT4 = 0x08; // 0000 1000
+
+    private static final int OFFSET_TO_FLAG_BYTE = 6;
+    private static final int OFFSET_TO_SFID = 3;
+
+    private static final int STRUCTURED_FIELD_INTRODUCER_LENGTH = 7;
+
+    private final Record record;
+    private final byte flagByte;
+    private final int sfid;
+
+    /**
+     * Constructs a {@code StructuredFieldIntroducer} from a read {@link Record}.
+     *
+     * @param record   the read {@link Record}.
+     */
+    StructuredFieldIntroducer(final Record record) {
+        this.record = record;
+
+        // It is very very likely that a method is called that requires the flag byte or the Structured Field
+        // Identifier. So we determine them here to get a better performance and more readable code...
+        final byte[] data = record.getData();
+
+        this.flagByte = data[OFFSET_TO_FLAG_BYTE];
+        this.sfid = ByteUtils.toInteger(data, OFFSET_TO_SFID, 3);
+    }
+
+    /**
+     * Returns the Structured Field Identifier (SFID). Constants for every known SFID can be found in the helper class
+     * {@link de.textmode.afpbox.common.StructuredFieldIdentifier}.
+     *
+     * @return the Structured Field Identifier (SFID).
+     */
+    public int getStructuredFieldIdentifier() {
+        return this.sfid;
+    }
+
+    /**
+     * Returns <code>true</code> if the Structured Field Introducer has an extension. If an extension
+     * is present, the Structured Field Introducer can be up to 255 bytes long. The actual length
+     * can be get by invoking {@link #getStructuredFieldIntroducerLength}.
+     *
+     * @return <code>true</code> if the Structured Field Introducer has an Extension.
+     */
+    public boolean hasStructuredFieldIntroducerExtension() {
+        return (this.flagByte & BIT0) != 0;
+    }
+
+    /**
+     * Returns <code>true</code> if the Structured Field has been segmented in multiple Structured
+     * Fields. Segmenting normally only occurs for those structured fields that contain OCA data.
+     *
+     * @return <code>true</code> if the Structured Field has been segmented.
+     */
+    public boolean isSegmented() {
+        return (this.flagByte & BIT2) != 0;
+    }
+
+    /**
+     * Returns <code>true</code> if the Structured Field contains padding data. Padding bytes may be
+     * used by an application to extend the physical length of a structured field.
+     *
+     * @return <code>true</code> if the Structured Field contains padding data.
+     */
+    public boolean hasPaddingData() {
+        return (this.flagByte & BIT4) != 0;
+    }
+
+    /**
+     * Returns the length of the padding data. If no padding data is present zero is returned.
+     *
+     * @return length of the padding data of zero if no padding data is present.
+     *
+     * @throws AfpException if the record specifies an invalid number of padding bytes.
+     */
+    public int getPaddingDataLength() throws AfpException {
+        if (!this.hasPaddingData()) {
+            return 0;
+        }
+
+        // If the last byte of the record contains 0x00, then the two bytes before this 0x00 contain
+        // the lengths of the padding bytes...
+        final int paddingLength;
+
+        final byte[] data = this.record.getData();
+        final int lastByte = ByteUtils.toInteger(data, data.length - 1, 1);
+
+        if (lastByte == 0x00) {
+            paddingLength = ByteUtils.toInteger(data, data.length - 3, 2);
+
+            if (paddingLength < 3 || paddingLength > 32759) {
+                throw new AfpException(
+                        "Record at offset " + this.record.getOffset()
+                        + " specifies an invalid length of padding bytes: " + paddingLength);
+            }
+        } else {
+            paddingLength = lastByte;
+        }
+
+        return paddingLength;
+    }
+
+    /**
+     * Returns the length of the Structured Field Introducer. The regular length of the Structured Field
+     * Introducer is seven bytes, but it may be longer (8 to 263 bytes) if an extension is present.
+     *
+     * @return length of the Structured Field Introducer.
+     *
+     * @throws AfpException if the record specifies an invalid extension length.
+     */
+    public int getStructuredFieldIntroducerLength() throws AfpException {
+        if (!this.hasStructuredFieldIntroducerExtension()) {
+            return STRUCTURED_FIELD_INTRODUCER_LENGTH;
+        }
+
+        final int extensionLength = ByteUtils.toInteger(this.record.getData(), 9, 1);
+        if (extensionLength < 1) {
+            throw new AfpException(
+                    "Record at offset " + this.record.getOffset()
+                    + " specifies an invalid extension length: " + extensionLength);
+        }
+
+        return STRUCTURED_FIELD_INTRODUCER_LENGTH + extensionLength;
+    }
+}
---
+++ afpbox/src/test/java/de/textmode/afpbox/StructuredFieldIntroducerTest.java
@@ -0,0 +1,170 @@
+package de.textmode.afpbox;
+
+/*
+ * Copyright 2019 Michael Knigge
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.commons.codec.binary.Hex;
+
+import de.textmode.afpbox.common.StructuredFieldIdentifier;
+import de.textmode.afpbox.io.Record;
+import junit.framework.TestCase;
+
+public final class StructuredFieldIntroducerTest extends TestCase {
+
+    /**
+     * Checks a standard structured field.
+     */
+    public void testStandardStructuredField() throws Exception {
+        final byte[] beginPrintFile = Hex.decodeHex("5A0008D3A8A5000000".toCharArray());
+        final Record record = new Record(beginPrintFile, 123);
+
+        final StructuredFieldIntroducer sfi = new StructuredFieldIntroducer(record);
+        assertFalse(sfi.hasStructuredFieldIntroducerExtension());
+        assertFalse(sfi.isSegmented());
+        assertFalse(sfi.hasPaddingData());
+        assertEquals(sfi.getPaddingDataLength(), 0);
+        assertEquals(sfi.getStructuredFieldIntroducerLength(), 7);
+        assertEquals(sfi.getStructuredFieldIdentifier(), StructuredFieldIdentifier.BPF);
+    }
+
+    /**
+     * Checks a structured field with padding data (length two bytes).
+     */
+    public void testStructuredFieldWithShortPaddingData() throws Exception {
+        final byte[] beginPrintFile = Hex.decodeHex("5A000AD3A8A5080000FF02".toCharArray());
+        final Record record = new Record(beginPrintFile, 123);
+
+        final StructuredFieldIntroducer sfi = new StructuredFieldIntroducer(record);
+        assertFalse(sfi.hasStructuredFieldIntroducerExtension());
+        assertFalse(sfi.isSegmented());
+        assertTrue(sfi.hasPaddingData());
+        assertEquals(sfi.getPaddingDataLength(), 2);
+        assertEquals(sfi.getStructuredFieldIntroducerLength(), 7);
+        assertEquals(sfi.getStructuredFieldIdentifier(), StructuredFieldIdentifier.BPF);
+    }
+
+    /**
+     * Checks a structured field with padding data (length 255 bytes).
+     */
+    public void testStructuredFieldWithLongerShortPaddingData() throws Exception {
+        final byte[] beginPrintFile = Hex.decodeHex("5A000AD3A8A5080000FFFF".toCharArray());
+        final Record record = new Record(beginPrintFile, 123);
+
+        final StructuredFieldIntroducer sfi = new StructuredFieldIntroducer(record);
+        assertFalse(sfi.hasStructuredFieldIntroducerExtension());
+        assertFalse(sfi.isSegmented());
+        assertTrue(sfi.hasPaddingData());
+        assertEquals(sfi.getPaddingDataLength(), 255); // To check if the data is treated unsigned!
+        assertEquals(sfi.getStructuredFieldIntroducerLength(), 7);
+        assertEquals(sfi.getStructuredFieldIdentifier(), StructuredFieldIdentifier.BPF);
+    }
+
+    /**
+     * Checks a structured field with padding data (length 511 (0x01FF) bytes).
+     */
+    public void testStructuredFieldWithLongPaddingData() throws Exception {
+        final byte[] beginPrintFile = Hex.decodeHex("5A000BD3A8A508000001FF00".toCharArray());
+        final Record record = new Record(beginPrintFile, 123);
+
+        final StructuredFieldIntroducer sfi = new StructuredFieldIntroducer(record);
+        assertFalse(sfi.hasStructuredFieldIntroducerExtension());
+        assertFalse(sfi.isSegmented());
+        assertTrue(sfi.hasPaddingData());
+        assertEquals(sfi.getPaddingDataLength(), 511);
+        assertEquals(sfi.getStructuredFieldIntroducerLength(), 7);
+        assertEquals(sfi.getStructuredFieldIdentifier(), StructuredFieldIdentifier.BPF);
+    }
+
+    /**
+     * Checks a structured field with invalid padding data.
+     */
+    public void testStructuredFieldWithLongInvalidPaddingData() throws Exception {
+        final byte[] beginPrintFile = Hex.decodeHex("5A000BD3A8A5080000000200".toCharArray());
+        final Record record = new Record(beginPrintFile, 123);
+
+        final StructuredFieldIntroducer sfi = new StructuredFieldIntroducer(record);
+        try {
+            sfi.getPaddingDataLength();
+            fail("should fail because long padding data must be at least 3 bytes long");
+        } catch (final AfpException e) {
+            assertTrue(e.getMessage().contains("invalid length of padding"));
+        }
+    }
+
+    /**
+     * Checks a structured field with invalid padding data.
+     */
+    public void testStructuredFieldWithTooLongInvalidPaddingData() throws Exception {
+        final byte[] beginPrintFile = Hex.decodeHex("5A000BD3A8A5080000EFFF00".toCharArray());
+        final Record record = new Record(beginPrintFile, 123);
+
+        final StructuredFieldIntroducer sfi = new StructuredFieldIntroducer(record);
+        try {
+            sfi.getPaddingDataLength();
+            fail("should fail because long padding data is too long");
+        } catch (final AfpException e) {
+            assertTrue(e.getMessage().contains("invalid length of padding"));
+        }
+    }
+
+    /**
+     * Checks a structured field with a structured field extension.
+     */
+    public void testStructuredFieldExtension() throws Exception {
+        final byte[] beginPrintFile = Hex.decodeHex("5A000BD3A8A5800000030000".toCharArray());
+        final Record record = new Record(beginPrintFile, 123);
+
+        final StructuredFieldIntroducer sfi = new StructuredFieldIntroducer(record);
+        assertTrue(sfi.hasStructuredFieldIntroducerExtension());
+        assertFalse(sfi.isSegmented());
+        assertFalse(sfi.hasPaddingData());
+        assertEquals(sfi.getPaddingDataLength(), 0);
+        assertEquals(sfi.getStructuredFieldIntroducerLength(), 7 + 3);
+        assertEquals(sfi.getStructuredFieldIdentifier(), StructuredFieldIdentifier.BPF);
+    }
+
+    /**
+     * Checks a structured field with a invalid structured field extension.
+     */
+    public void testInvalidStructuredFieldExtension() throws Exception {
+        final byte[] beginPrintFile = Hex.decodeHex("5A000BD3A8A5800000000000".toCharArray());
+        final Record record = new Record(beginPrintFile, 123);
+
+        final StructuredFieldIntroducer sfi = new StructuredFieldIntroducer(record);
+        try {
+            sfi.getStructuredFieldIntroducerLength();
+            fail("should fail because an invalid extension length is specified");
+        } catch (final AfpException e) {
+            assertTrue(e.getMessage().contains("invalid extension length"));
+        }
+    }
+
+    /**
+     * Checks if the "is segmented" flag is detected.
+     */
+    public void testStructuredFieldSegmentation() throws Exception {
+        final byte[] beginPrintFile = Hex.decodeHex("5A0008D3A8A5200000".toCharArray());
+        final Record record = new Record(beginPrintFile, 123);
+
+        final StructuredFieldIntroducer sfi = new StructuredFieldIntroducer(record);
+        assertFalse(sfi.hasStructuredFieldIntroducerExtension());
+        assertTrue(sfi.isSegmented());
+        assertFalse(sfi.hasPaddingData());
+        assertEquals(sfi.getPaddingDataLength(), 0);
+        assertEquals(sfi.getStructuredFieldIntroducerLength(), 7);
+        assertEquals(sfi.getStructuredFieldIdentifier(), StructuredFieldIdentifier.BPF);
+    }
+}
