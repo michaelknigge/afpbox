07ea8067ba7be159d709431928edcdf6e281816e
Michael Knigge <michael.knigge@gmx.de>
2019-08-15 14:55:23 +0200

    Added a AfpDataInputStream class that will make parsing the contents of Structured Fields very easy.

---
+++ afpbox/src/main/java/de/textmode/afpbox/io/AfpDataInputStream.java
@@ -0,0 +1,279 @@
+package de.textmode.afpbox.io;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+
+/*
+ * Copyright 2019 Michael Knigge
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import de.textmode.afpbox.AfpException;
+
+/**
+ * The {@link AfpDataInputStream} is not really an DataInputStream, but it follows the concept of the DataInputStream. It provides
+ * methods for reading native data types and data types specific for AFP.
+ */
+public final class AfpDataInputStream {
+
+    private static final Charset EBCDIC_INTERNATIONAL = Charset.forName("ibm-500");
+
+    private final byte[] data;
+    private int offset;
+    private int bytesLeft;
+
+
+    /**
+     * Constructs an {@link AfpDataInputStream}.
+     *
+     * @param data the underlying byte array
+     * @param startOffset the start offset (usually the first byte after the Structured field introducer)
+     * @param paddingBytesToIgnore Padding bytes that shall be ignored
+     */
+    public AfpDataInputStream(final byte[] data, final int startOffset, final int paddingBytesToIgnore) {
+        this.data = data;
+        this.offset = startOffset;
+        this.bytesLeft = data.length - startOffset - paddingBytesToIgnore;
+    }
+
+    /**
+     * Checks if enough bytes are available to read. If not an {@link AfpException} is thrown with information that may help the
+     * user to validate the AFP structured field.
+     *
+     * @param length the required amount of bytes
+     *
+     * @throws AfpException if there are less then <code>length</code> bytes left to read.
+     */
+    private void checkForAvailableBytes(final int length) throws AfpException {
+        if (length > this.bytesLeft) {
+            throw new AfpException(
+                    "Tried to parse " + length +
+                    " bytes at offset " + this.offset +
+                    " but there are only " + this.bytesLeft +
+                    " bytes left to parse. Maybe the structured field is truncated.");
+        }
+    }
+
+    /**
+     * Reads and returns some bytes.
+     *
+     * @return the requested amount of bytes read.
+     *
+     * @throws AfpException if there are not enough bytes left to read.
+     */
+    public byte[] readBytes(final int length) throws AfpException {
+        this.checkForAvailableBytes(length);
+
+        final byte[] result = new byte[length];
+        System.arraycopy(this.data, this.offset, result, 0, length);
+
+        this.offset += length;
+        this.bytesLeft -= length;
+
+        return result;
+    }
+
+    /**
+     * Reads and returns some bytes and treads the bytes as an EBCDIC-International encoded String.
+     *
+     * @return the String decoded from the read bytes.
+     *
+     * @throws AfpException if there are not enough bytes left to read.
+     */
+    public String readEbcdicString(final int length) throws AfpException {
+        this.checkForAvailableBytes(length);
+
+        final String result = EBCDIC_INTERNATIONAL.decode(ByteBuffer.wrap(this.data, this.offset, length)).toString();
+
+        this.offset += length;
+        this.bytesLeft -= length;
+
+        return result;
+    }
+
+    /**
+     * Reads and returns some bytes and treads the bytes as an encoded single byte character String.
+     *
+     * @return the String decoded from the read bytes.
+     *
+     * @throws AfpException if there are not enough bytes left to read.
+     */
+    public String readString(final int length, final Charset charset) throws AfpException {
+        this.checkForAvailableBytes(length);
+
+        final String result = charset.decode(ByteBuffer.wrap(this.data, this.offset, length)).toString();
+
+        this.offset += length;
+        this.bytesLeft -= length;
+
+        return result;
+    }
+    
+    /**
+     * Reads and returns one input byte. The byte is treated as an unsigned value.
+     *
+     * @return the unsigned 8-bit value read.
+     *
+     * @throws AfpException if there are not enough bytes left to parse.
+     */
+    public int readUnsignedByte() throws AfpException {
+        this.checkForAvailableBytes(1);
+
+        final int result = this.data[this.offset] & 0xFF;
+
+        ++this.offset;
+        --this.bytesLeft;
+
+        return result;
+    }
+
+    /**
+     * Reads and returns one input byte. The byte is treated as an signed value.
+     *
+     * @return the signed 8-bit value read.
+     *
+     * @throws AfpException if there are not enough bytes left to parse.
+     */
+    public int readByte() throws AfpException {
+        this.checkForAvailableBytes(1);
+
+        final int result = this.data[this.offset];
+
+        ++this.offset;
+        --this.bytesLeft;
+
+        return result;
+    }
+
+    /**
+     * Reads two input bytes and returns a integer value. The value is treated as an unsigned value.
+     *
+     * @return the unsigned 16-bit value read.
+     *
+     * @throws AfpException if there are not enough bytes left to parse.
+     */
+    public int readUnsignedInteger16() throws AfpException {
+        this.checkForAvailableBytes(2);
+
+        final int result = ((this.data[this.offset] & 0xFF) << 8) | (this.data[this.offset + 1] & 0xFF);
+
+        this.offset += 2;
+        this.bytesLeft -= 2;
+
+        return result;
+    }
+
+    /**
+     * Reads two input bytes and returns a integer value. The value is treated as an signed value.
+     *
+     * @return the signed 16-bit value read.
+     *
+     * @throws AfpException if there are not enough bytes left to parse.
+     */
+    public int readInteger16() throws AfpException {
+        this.checkForAvailableBytes(2);
+
+        final int result = (this.data[this.offset] << 8) | (this.data[this.offset + 1] & 0xFF);
+
+        this.offset += 2;
+        this.bytesLeft -= 2;
+
+        return result;
+    }
+
+    /**
+     * Reads three input bytes and returns a integer value. The value is treated as an unsigned value.
+     *
+     * @return the unsigned 24-bit value read.
+     *
+     * @throws AfpException if there are not enough bytes left to parse.
+     */
+    public int readUnsignedInteger24() throws AfpException {
+        this.checkForAvailableBytes(3);
+
+        final int result =
+                ((this.data[this.offset] & 0xFF) << 16) |
+                ((this.data[this.offset + 1] & 0xFF) << 8) |
+                (this.data[this.offset + 2] & 0xFF);
+
+        this.offset += 3;
+        this.bytesLeft -= 3;
+
+        return result;
+    }
+
+    /**
+     * Reads three input bytes and returns a integer value. The value is treated as an signed value.
+     *
+     * @return the signed 24-bit value read.
+     *
+     * @throws AfpException if there are not enough bytes left to parse.
+     */
+    public int readInteger24() throws AfpException {
+        this.checkForAvailableBytes(3);
+
+        final int result =
+                (this.data[this.offset] << 16) |
+                ((this.data[this.offset + 1] & 0xFF) << 8) |
+                (this.data[this.offset + 2] & 0xFF);
+
+        this.offset += 3;
+        this.bytesLeft -= 3;
+
+        return result;
+    }
+
+    /**
+     * Reads four input bytes and returns a long value. The value is treated as an unsigned value.
+     *
+     * @return the unsigned 32-bit value read.
+     *
+     * @throws AfpException if there are not enough bytes left to parse.
+     */
+    public long readUnsignedInteger32() throws AfpException {
+        this.checkForAvailableBytes(4);
+
+        final long result =
+                ((long) (this.data[this.offset] & 0xFF) << 24) |
+                (long) (this.data[this.offset + 1] & 0xFF) << 16 |
+                (long) (this.data[this.offset + 2] & 0xFF) << 8 |
+                (long) this.data[this.offset + 3] & 0xFF;
+
+        this.offset += 4;
+        this.bytesLeft -= 4;
+
+        return result;
+    }
+
+    /**
+     * Reads four input bytes and returns a integer value. The value is treated as an signed value.
+     *
+     * @return the signed 32-bit value read.
+     *
+     * @throws AfpException if there are not enough bytes left to parse.
+     */
+    public int readInteger32() throws AfpException {
+        this.checkForAvailableBytes(4);
+
+        final int result =
+                (this.data[this.offset] << 24) |
+                ((this.data[this.offset + 1] & 0xFF) << 16) |
+                ((this.data[this.offset + 2] & 0xFF) << 8) |
+                (this.data[this.offset + 3] & 0xFF);
+
+        this.offset += 4;
+        this.bytesLeft -= 4;
+
+        return result;
+    }
+}
---
+++ afpbox/src/test/java/de/textmode/afpbox/io/AfpDataInputStreamTest.java
@@ -0,0 +1,187 @@
+package de.textmode.afpbox.io;
+
+import java.nio.charset.Charset;
+import java.util.Arrays;
+
+/*
+ * Copyright 2018 Michael Knigge
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
+import de.textmode.afpbox.AfpException;
+import junit.framework.TestCase;
+
+/**
+ * Unit-Tests for the class {@link AfpDataInputStreamTest}.
+ */
+public final class AfpDataInputStreamTest extends TestCase {
+
+    /**
+     * Helper Method for creating a {@link AfpDataInputStream}.
+     */
+    private static AfpDataInputStream buildAfpDataInputStream(final String hexString) throws DecoderException {
+        return new AfpDataInputStream(Hex.decodeHex(hexString.toCharArray()), 0, 0);
+    }
+
+    /**
+     * Checks reading unsigned 8 bit values.
+     */
+    public void testReadUnsignedByte() throws Exception {
+        assertEquals(0, buildAfpDataInputStream("00").readUnsignedByte());
+        assertEquals(1, buildAfpDataInputStream("01").readUnsignedByte());
+        assertEquals(128, buildAfpDataInputStream("80").readUnsignedByte());
+        assertEquals(255, buildAfpDataInputStream("FF").readUnsignedByte());
+    }
+
+    /**
+     * Checks reading signed 8 bit values.
+     */
+    public void testReadSignedByte() throws Exception {
+        assertEquals(0, buildAfpDataInputStream("00").readByte());
+        assertEquals(1, buildAfpDataInputStream("01").readByte());
+        assertEquals(-128, buildAfpDataInputStream("80").readByte());
+        assertEquals(-1, buildAfpDataInputStream("FF").readByte());
+    }
+
+    /**
+     * Checks reading unsigned 16 bit values.
+     */
+    public void testReadUnsignedInteger16() throws Exception {
+        assertEquals(0, buildAfpDataInputStream("0000").readUnsignedInteger16());
+        assertEquals(1, buildAfpDataInputStream("0001").readUnsignedInteger16());
+        assertEquals(512, buildAfpDataInputStream("0200").readUnsignedInteger16());
+        assertEquals(50176, buildAfpDataInputStream("C400").readUnsignedInteger16());
+        assertEquals(65535, buildAfpDataInputStream("FFFF").readUnsignedInteger16());
+    }
+
+    /**
+     * Checks reading signed 16 bit values.
+     */
+    public void testReadSignedInteger16() throws Exception {
+        assertEquals(12345, buildAfpDataInputStream("3039").readInteger16());
+        assertEquals(12288, buildAfpDataInputStream("3000").readInteger16());
+        assertEquals(1024, buildAfpDataInputStream("0400").readInteger16());
+        assertEquals(512, buildAfpDataInputStream("0200").readInteger16());
+        assertEquals(1, buildAfpDataInputStream("0001").readInteger16());
+        assertEquals(0, buildAfpDataInputStream("0000").readInteger16());
+        assertEquals(-1, buildAfpDataInputStream("FFFF").readInteger16());
+        assertEquals(-512, buildAfpDataInputStream("FE00").readInteger16());
+        assertEquals(-1024, buildAfpDataInputStream("FC00").readInteger16());
+        assertEquals(-12288, buildAfpDataInputStream("D000").readInteger16());
+        assertEquals(-12345, buildAfpDataInputStream("CFC7").readInteger16());
+    }
+
+    /**
+     * Checks reading unsigned 24 bit values.
+     */
+    public void testReadUnsignedInteger24() throws Exception {
+        assertEquals(0, buildAfpDataInputStream("000000").readUnsignedInteger24());
+        assertEquals(1, buildAfpDataInputStream("000001").readUnsignedInteger24());
+        assertEquals(512, buildAfpDataInputStream("000200").readUnsignedInteger24());
+        assertEquals(12845056, buildAfpDataInputStream("C40000").readUnsignedInteger24());
+        assertEquals(16777215, buildAfpDataInputStream("FFFFFF").readUnsignedInteger24());
+    }
+
+    /**
+     * Checks reading signed 24 bit values.
+     */
+    public void testReadSignedInteger24() throws Exception {
+        assertEquals(0, buildAfpDataInputStream("000000").readInteger24());
+        assertEquals(1, buildAfpDataInputStream("000001").readInteger24());
+        assertEquals(512, buildAfpDataInputStream("000200").readInteger24());
+        assertEquals(-3932160, buildAfpDataInputStream("C40000").readInteger24());
+        assertEquals(-1, buildAfpDataInputStream("FFFFFF").readInteger24());
+    }
+
+    /**
+     * Checks reading unsigned 32 bit values.
+     */
+    public void testReadUnsignedInteger32() throws Exception {
+        assertEquals(12345, buildAfpDataInputStream("00003039").readUnsignedInteger32());
+        assertEquals(12288, buildAfpDataInputStream("00003000").readUnsignedInteger32());
+        assertEquals(1024, buildAfpDataInputStream("00000400").readUnsignedInteger32());
+        assertEquals(512, buildAfpDataInputStream("00000200").readUnsignedInteger32());
+        assertEquals(1, buildAfpDataInputStream("00000001").readUnsignedInteger32());
+        assertEquals(0, buildAfpDataInputStream("00000000").readUnsignedInteger32());
+        assertEquals(4294967295L, buildAfpDataInputStream("FFFFFFFF").readUnsignedInteger32());
+        assertEquals(4294966784L, buildAfpDataInputStream("FFFFFE00").readUnsignedInteger32());
+        assertEquals(4294966272L, buildAfpDataInputStream("FFFFFC00").readUnsignedInteger32());
+    }
+
+    /**
+     * Checks reading signed 32 bit values.
+     */
+    public void testReadSignedInteger32() throws Exception {
+        assertEquals(12345, buildAfpDataInputStream("00003039").readInteger32());
+        assertEquals(12288, buildAfpDataInputStream("00003000").readInteger32());
+        assertEquals(1024, buildAfpDataInputStream("00000400").readInteger32());
+        assertEquals(512, buildAfpDataInputStream("00000200").readInteger32());
+        assertEquals(1, buildAfpDataInputStream("00000001").readInteger32());
+        assertEquals(0, buildAfpDataInputStream("00000000").readInteger32());
+        assertEquals(-1, buildAfpDataInputStream("FFFFFFFF").readInteger32());
+        assertEquals(-512, buildAfpDataInputStream("FFFFFE00").readInteger32());
+        assertEquals(-1024, buildAfpDataInputStream("FFFFFC00").readInteger32());
+        assertEquals(-12288, buildAfpDataInputStream("FFFFD000").readInteger32());
+        assertEquals(-12345, buildAfpDataInputStream("FFFFCFC7").readInteger32());
+    }
+
+    /**
+     * Checks reading multiple values
+     */
+    public void testReadMultipleValues() throws Exception {
+        final AfpDataInputStream is = new AfpDataInputStream(
+                Hex.decodeHex("01000100000100000001F4F5F663FCF1F2F3".toCharArray()), 0, 0);
+
+        assertEquals(1, is.readUnsignedByte());
+        assertEquals(1, is.readUnsignedInteger16());
+        assertEquals(1, is.readUnsignedInteger24());
+        assertEquals(1, is.readUnsignedInteger32());
+        assertEquals("456", is.readEbcdicString(3));
+        assertEquals("[]", is.readString(2, Charset.forName("ibm-273")));
+        assertTrue(Arrays.equals(Hex.decodeHex("F1F2F3".toCharArray()), is.readBytes(3)));
+
+        // All bytes are consumed - reading even one more byte should throw an exception....
+        try {
+            is.readUnsignedByte();
+            fail("read after the end of the stream...");
+        } catch (final AfpException e) {
+            assertTrue(e.getMessage().contains("0 bytes left to parse."));
+        }
+    }
+
+    /**
+     * Checks reading multiple values (staring at non-zero offset and some padding bytes at the end)
+     */
+    public void testReadMultipleValuesWithPaddingBytesAndNonZeroOffset() throws Exception {
+        final AfpDataInputStream is = new AfpDataInputStream(Hex.decodeHex(
+                "FFFFFF01000100000100000001FFFF".toCharArray()), 3, 2);
+
+        assertEquals(1, is.readUnsignedByte());
+        assertEquals(1, is.readUnsignedInteger16());
+        assertEquals(1, is.readUnsignedInteger24());
+        assertEquals(1, is.readUnsignedInteger32());
+
+        // All bytes are consumed - reading even one more byte should throw an exception....
+        try {
+            is.readUnsignedByte();
+            fail("read after the end of the stream...");
+        } catch (final AfpException e) {
+            assertTrue(e.getMessage().contains("0 bytes left to parse."));
+        }
+    }
+
+}
